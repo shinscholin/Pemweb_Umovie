e72abe43af05e9b6041db380c346d0b896bb6acf
shinscholin <adryansyachpc@gmail.com>
2020-05-25 00:51:36 +0700

    coba coba pakai sublime merge

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Multi-User Authentication Tutorail
 
 _Produced by DevMarketer_
+# Pemweb_Umovie
+# Pemweb_Umovie
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .postCss('resources/css/main.css', 'public/css', [
+   	require('tailwindcss'),
+   ])
+
